dc2a60f3477e65e0f9e0de17499d7887970a6982
ludovic.regnier@edu.esiee.fr <>
2025-03-11 09:33:24 +0100

    Programming Essentials - Create a rotating collectible

---
+++ Assets/_Unity Essentials/Scripts/Provided Scripts/Collectible.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+    public float rotationSpeed = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, rotationSpeed, 0);
+    }
+}
